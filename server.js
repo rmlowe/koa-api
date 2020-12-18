2590023f1771746666bcfd413752eff243b2de6f
Robert Lowe <robertmlowe@rmlowe.com>
2020-12-18 17:21:50 +0800

    Challenge: GET Posts Endpoint

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
 const PORT = 4000;
-//import the koa-router
-const Router = require('koa-router');
 const router = new Router();
 
-
-
-//create a root route
+const posts = [
+    {
+        "id": '1',
+        "name": "Nodejs Developer",
+        "content": "ajkshdkjashdk ajshdkjasd"
+    },
+    {
+        "id": '2',
+        "name": "Sailsjs Developer",
+        "content": "ajkshdkjashdk ajshdkjasd"
+    },
+    {
+        "id": '3',
+        "name": "Vuejs Developer",
+        "content": "ajkshdkjashdk ajshdkjasd"
+    }
+];
 router.get('/', (ctx) => {
     ctx.body = 'welcome to koa application';
+})
+
+//GET /posts
+router.get('/posts', (ctx) => {
+    ctx.body = posts;
 })
 
 app.use(router.routes());
